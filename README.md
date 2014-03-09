724e5d3652a4c43ded6871dda37a6a91213d8f17
yuxiang <yuxiangster@gmail.com>
2014-03-09 22:17:45 +0800

    modify readme

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ TextEditor
 ==========
 
 This a project based on Qt5.2.
+
+1) Run configure.sh to generate Makefile or XCode project.
+2) Run make to build.
--- configure.sh
+++ configure.sh
@@ -2,6 +2,11 @@
 rm -rf build
 mkdir build
 cd build
-cmake -GXcode -DCMAKE_BUILD_TYPE=Debug ..
-
+OS=`uname`
+if [ $OS = "Darwin" ]; then
+    echo "This is Mac OS X."
+    cmake -GXcode -DCMAKE_BUILD_TYPE=Debug ..
+else
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+fi
 
